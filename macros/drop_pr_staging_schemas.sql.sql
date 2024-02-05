03f80b4d9b7902c492c729b3b923916bda9073ab
Sung Won Chung <sung@datafold.com>
2024-02-05 14:30:02 -0800

    add templates

---
+++ macros/drop_pr_staging_schemas.sql.sql
@@ -0,0 +1,27 @@
+{%- macro drop_pr_staging_schemas(PR_number) %}
+
+    {% set pr_cleanup_query %}
+        with pr_staging_schemas as (
+            select schema_name
+            from analytics.information_schema.schemata
+            where
+            schema_name like 'PR_NUM_'||{{ PR_number }}||'%'
+        )
+
+        select 
+            'drop schema analytics.'||schema_name||';' as drop_command 
+        from pr_staging_schemas
+    {% endset %}
+
+    {% do log(pr_cleanup_query, info=TRUE) %}
+
+    {% set drop_commands = run_query(pr_cleanup_query).columns[0].values() %}
+
+    {% if drop_commands %}
+        {% for drop_command in drop_commands %}
+            {% do log(drop_command, True) %} {% do run_query(drop_command) %}
+        {% endfor %}
+    {% else %} {% do log('No schemas to drop.', True) %}
+    {% endif %}
+
+{%- endmacro -%}
