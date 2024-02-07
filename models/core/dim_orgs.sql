9caf5cdefbba52f83a8f5c6770009c6a8998cc1d
Sung Won Chung <sungwonchung3@gmail.com>
2024-02-08 09:28:11 +1300

    Revert "test changes (#5)" (#6)
    This reverts commit c2c29cee30807896e220ac01b830383319c3932f.

--- models/core/dim_orgs.sql
+++ models/core/dim_orgs.sql
@@ -2,10 +2,9 @@ WITH orgs AS (
 --prod
     SELECT
         org_id
-        , org_name
-        , employee_range
-        , created_at
-    FROM {{ ref('org_created') }}
+        , MIN(event_timestamp) AS created_at
+    FROM {{ ref('signed_in') }}
+    GROUP BY 1
 
 -- --dev
 --    SELECT
@@ -29,7 +28,7 @@ WITH orgs AS (
         org_id
         , event_timestamp AS sub_created_at
         , plan as sub_plan
-        , coalesce(price, 0) as sub_price
+        , price as sub_price
     FROM {{ ref('subscription_created') }}
 )
 
@@ -39,8 +38,8 @@ SELECT
     , created_at
     , num_users
     , sub_created_at
-    , case when num_users = 1 then 'Individual' else sub_plan end as sub_plan
+    , sub_plan
     , sub_price
 FROM orgs
 LEFT JOIN user_count USING (org_id)
-LEFT JOIN subscriptions USING (org_id)
+LEFT JOIN subscriptions USING (org_id) 
